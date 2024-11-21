3c9a0cd5629ad358ba3e3b125fd97f8d94ff34c6
Fernando Blat <fernando@blat.es>
2024-11-21 11:42:42 +0100

    Validate numeric items to be less than 100

--- lib/section_extractor/toc.rb
+++ lib/section_extractor/toc.rb
@@ -4,6 +4,8 @@ module SectionExtractor
   class Toc
     attr_accessor :toc_series_type, :toc_separator_chars, :toc_items
 
+    MAX_NUMERIC_TOC_ITEM = 1000
+
     def initialize(toc_series_type, toc_separator_chars)
       @toc_items = []
       # The type of toc series can be:
@@ -15,7 +17,18 @@ module SectionExtractor
     end
 
     def add_item(raw_title, position)
-      @toc_items << TocItem.new(raw_title, raw_title, position)
+      toc_item = TocItem.new(raw_title, raw_title, position)
+      @toc_items << toc_item if valid_toc_item?(toc_item)
+    end
+
+    private
+
+    def valid_toc_item?(toc_item)
+      case @toc_series_type
+      when :numeric
+        match = toc_item.raw_title.match(/^(\d+)/)
+        match.size > 0 && match[1].to_i <= MAX_NUMERIC_TOC_ITEM
+      end
     end
   end
 end
